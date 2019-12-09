0f2ea20105d71c0d95fe99c36a3d5048bb6cd7ef
Troy Hall <programming@hallhome.us>
2019-12-09 10:13:17 -0600

    Revert "addded wrapper per issue #1 if they want the background full width they can manually add it in their css. Add jumbotron via module class."
    This reverts commit 25239b42

--- tmpl/default.php
+++ tmpl/default.php
@@ -50,7 +50,7 @@ HTMLHelper::_('stylesheet', $modPath . 'css/bear.carousel.css');
 $document->addStyleDeclaration('.set_testimony { margin:' . $params->get('container_fix', 6) . 'px; }');
 
 // Get background params
-$style = ".bears-container{\n";
+$style = "div#testimonials, div#testimonials-wide{\n";
 $style .= $modulebackgroundImage ? " background-image: url(\"" . $modulebackgroundImage . "\");\n" : '';
 $style .= " background-repeat: " . $modulebackgroundRepeat . ";\n";
 $style .= $modulebackgroundColor ? " background-color: " . $modulebackgroundColor . ";\n" : '';
@@ -59,47 +59,46 @@ $style .= "}\n";
 $document->addStyleDeclaration($style);
 
 ?>
-<div class = "bears-container">
-	<?php if ( $params->get('jpreload') == '1' ) : ?>
-		<script type = "text/javascript">
-            function preload() {
-                document.getElementById("loaded").style.display = "none";
-                document.getElementById("bears-testimonial").style.display = "block";
-            }//preloader
-            window.onload = preload;
-		</script>
-		<div id = "loaded"></div>
-	<?php endif; ?>
 
-	<div class = "owl-carousel <?php echo $params->get('navStyle') ?> <?php echo $params->get('navPosit') ?> <?php echo $params->get('navRounded') ?>"
-	     data-dots = "false" data-autoplay = "<?php echo $params->get('autoplay') ?>"
-	     data-autoplay-hover-pause = "<?php echo $params->get('autoplay-hover-pause') ?>"
-	     data-autoplay-timeout = "<?php echo $params->get('autoplay-timeout') ?>"
-	     data-autoplay-speed = "<?php echo $params->get('autoplay-speed') ?>"
-	     data-loop = "<?php echo $params->get('dataLoop') ?>" data-nav = "<?php echo $params->get('dataNav') ?>"
-	     data-nav-speed = "<?php echo $params->get('autoplay-speed') ?>"
-	     data-items = "<?php echo $params->get('image_width') ?>"
-	     data-tablet-landscape = "<?php echo $params->get('image_width_tabl') ?>"
-	     data-tablet-portrait = "<?php echo $params->get('image_width_tabp') ?>"
-	     data-mobile-landscape = "<?php echo $params->get('image_width_mobl') ?>"
-	     data-mobile-portrait = "<?php echo $params->get('image_width_mobp') ?>">
-		<?php foreach ( $testimonials_items as $item ) : ?>
-			<div class = "set_testimony">
-				<div class = "reviews-block__slide">
-					<div class = "reviews-block__text"><?php echo $item->info; ?></div>
-					<div class = "reviews-block__person">
-						<?php if ( $item->img ) : ?>
-							<div class = "reviews-block__person-image">
-								<img src = "<?php echo $item->img; ?>" alt = "<?php echo $item->name; ?>">
-							</div>
-						<?php endif; ?>
-						<div class = "reviews-block__person-data">
-							<div class = "reviews-block__person-name"><?php echo $item->name; ?></div>
-							<div class = "reviews-block__person-role"><?php echo $item->title; ?></div>
+<?php if ( $params->get('jpreload') == '1' ) : ?>
+	<script type = "text/javascript">
+        function preload() {
+            document.getElementById("loaded").style.display = "none";
+            document.getElementById("bears-testimonial").style.display = "block";
+        }//preloader
+        window.onload = preload;
+	</script>
+	<div id = "loaded"></div>
+<?php endif; ?>
+
+<div class = "owl-carousel <?php echo $params->get('navStyle') ?> <?php echo $params->get('navPosit') ?> <?php echo $params->get('navRounded') ?>"
+     data-dots = "false" data-autoplay = "<?php echo $params->get('autoplay') ?>"
+     data-autoplay-hover-pause = "<?php echo $params->get('autoplay-hover-pause') ?>"
+     data-autoplay-timeout = "<?php echo $params->get('autoplay-timeout') ?>"
+     data-autoplay-speed = "<?php echo $params->get('autoplay-speed') ?>"
+     data-loop = "<?php echo $params->get('dataLoop') ?>" data-nav = "<?php echo $params->get('dataNav') ?>"
+     data-nav-speed = "<?php echo $params->get('autoplay-speed') ?>"
+     data-items = "<?php echo $params->get('image_width') ?>"
+     data-tablet-landscape = "<?php echo $params->get('image_width_tabl') ?>"
+     data-tablet-portrait = "<?php echo $params->get('image_width_tabp') ?>"
+     data-mobile-landscape = "<?php echo $params->get('image_width_mobl') ?>"
+     data-mobile-portrait = "<?php echo $params->get('image_width_mobp') ?>">
+	<?php foreach ( $testimonials_items as $item ) : ?>
+		<div class = "set_testimony">
+			<div class = "reviews-block__slide">
+				<div class = "reviews-block__text"><?php echo $item->info; ?></div>
+				<div class = "reviews-block__person">
+					<?php if ( $item->img ) : ?>
+						<div class = "reviews-block__person-image">
+							<img src = "<?php echo $item->img; ?>" alt = "<?php echo $item->name; ?>">
 						</div>
+					<?php endif; ?>
+					<div class = "reviews-block__person-data">
+						<div class = "reviews-block__person-name"><?php echo $item->name; ?></div>
+						<div class = "reviews-block__person-role"><?php echo $item->title; ?></div>
 					</div>
 				</div>
 			</div>
-		<?php endforeach; ?>
-	</div>
+		</div>
+	<?php endforeach; ?>
 </div>
